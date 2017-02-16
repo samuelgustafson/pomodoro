e64f9853fc13eb62acdfcb6a55dd16004d5e8d8f
Justin Dennison <justin1dennison@gmail.com>
2017-02-15 20:07:23 -0500

    created a basic interval timer that is started with a button click and runs every second. Additionally, I have cached the DOM elements for future use

---
+++ public/js/app.js
@@ -0,0 +1,23 @@
+// Data and Variable Declarations
+var timer;
+var minutesLeft = 25;
+var secondsLeft = 0;
+var isOnBreak = false;
+var numberOfBreaks = 0;
+// Getting references the DOM
+var minutes = document.querySelector('#minutes');
+var seconds = document.querySelector('#seconds');
+var startButton = document.querySelector('#start');
+// Initialization Code
+  //EventListeners
+  startButton.addEventListener('click', start);
+// Function Definitions
+function start(){
+  if(!timer){
+      timer = setInterval(tick, 1000);
+  }
+}
+function tick(){
+  console.log('tick');
+}
+function render(){}
