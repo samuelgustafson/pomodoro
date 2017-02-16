e54b5af2d75278edb058835d120eebd7ae57db44
Justin Dennison <justin1dennison@gmail.com>
2017-02-15 20:57:15 -0500

    Accurately decreases minutes and seconds after start is click. Ensures that appropriate two digit format is used for minutes and seconds less than 10.

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,7 @@
 // Data and Variable Declarations
 var timer;
-var minutesLeft = 25;
-var secondsLeft = 0;
+var minutesLeft = 10;
+var secondsLeft = 10;
 var isOnBreak = false;
 var numberOfBreaks = 0;
 // Getting references the DOM
@@ -11,6 +11,7 @@ var startButton = document.querySelector('#start');
 // Initialization Code
   //EventListeners
   startButton.addEventListener('click', start);
+  render();
 // Function Definitions
 function start(){
   if(!timer){
@@ -18,6 +19,31 @@ function start(){
   }
 }
 function tick(){
-  console.log('tick');
+  decrementMinutes();
+  decrementSeconds();
+  render();
 }
-function render(){}
+function decrementMinutes(){
+  if(secondsLeft === 0){
+    minutesLeft -= 1;
+  }
+}
+function decrementSeconds(){
+  if(secondsLeft === 0){
+    secondsLeft = 59;
+  } else {
+    secondsLeft -= 1;
+  }
+}
+function render(){
+  minutes.textContent = pad(minutesLeft);
+  seconds.textContent = pad(secondsLeft);
+}
+
+function pad(num){
+  if(num < 10){
+    return `0${num}`;
+  } else {
+    return num;
+  }
+}
